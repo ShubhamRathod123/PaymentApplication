686681e2aa28eb3f35ed54ed0a1d455ceca86b74
Tejas  Jahagirdar <jahagirdartejas181@gmail.com>
2025-08-28 14:36:15 +0530

    Initial commit

---
+++ quickfunds/quickfunds/src/test/java/com/app/quickfunds/QuickfundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quickfunds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
